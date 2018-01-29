c9b7be91454780510c8246300699ac46ca5dad7e
Devon Johnson <dseth.johnson@GMAIL.COM>
2018-01-29 12:28:23 -0500

    seeds work

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,9 +2,9 @@
 const express = require('express')
 const Router = express.Router()
 const Book = require('../models/BookSchema')
-module.exports = Router
 
-router.get('/', (req, res) => {
+
+Router.get('/', (req, res) => {
   Book.find({})
     .then((book) => {
       res.json(book)
@@ -13,3 +13,4 @@ router.get('/', (req, res) => {
       console.log(err)
     })
 })
+module.exports = Router
