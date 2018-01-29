d96ca2459c781d634c485d9475cd1c95d0209357
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-01-29 12:09:00 -0500

    different controller

---
+++ controllers/bookController.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const Router = express.Router()
+const mongoose = require('mongoose')
+const Book = require('../models/BookSchema.js')
+
+
+
+
+
+
+
+
+
+module.exports = Router
