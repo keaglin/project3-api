15f1fca350bfbd5f213d71b22b2cb0f9f175b8ef
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-01-29 12:21:34 -0500

    set to port 3000

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,14 +2,16 @@
 const express = require('express')
 const Router = express.Router()
 const Book = require('../models/BookSchema')
-module.exports = Router
 
-router.get('/', (req, res) => {
-  Book.find({})
+Router.get('/', (req, res) => {
+    Book.find({})
     .then((book) => {
-      res.json(book)
+        res.json(book)
     })
     .catch((err) => {
-      console.log(err)
+        console.log(err)
     })
 })
+
+
+module.exports = Router
