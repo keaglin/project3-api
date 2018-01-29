48d05ccd6de61528b793ff0b05ed34fac0ff48ee
Devon Johnson <dseth.johnson@GMAIL.COM>
2018-01-29 14:29:43 -0500

    update

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,74 +1,150 @@
 
+// const express = require('express')
+// const Router = express.Router()
+// const Book = require('../models/BookSchema')
+
+// Router.get('/', (req, res) => {
+//   Book.find({})
+//     .then((book) => {
+//       res.json(book)
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
+// })
+
+// Router.get('/:title', (req, res) => {
+//   Book.find({
+//     title: req.params.title
+//   })
+//       .then((book) => {
+//         res.json(book)
+//         console.log(book)
+//       })
+//       .catch((err) => {
+//         console.log(err)
+//       })
+// })
+
+// Router.get('/:author', (req, res) => {
+//   Book.find({
+//     author: req.params.author
+//   })
+//         .then((book) => {
+//           res.json(book)
+//         })
+//         .catch((err) => {
+//           console.log(err)
+//         })
+// })
+
+// Router.post('/:title', (req, res) => {
+//   Book.create(req.body.book)
+//           .then((book) => {
+//             res.redirect(`/books/${book.title}`)
+//           })
+//           .catch((err) => {
+//             console.log(err)
+//           })
+// })
+
+// Router.put('/:title', (req, res) => {
+//   Book.findOneAndUpdate({
+//     title: req.params.title
+//   }, req.body.book, {new: true})
+//         .then((book) => {
+//           res.redirect(`/books/${book.title}`)
+//         })
+//         .catch((err) => {
+//           console.log(err)
+//         })
+// })
+
+// Router.delete('/:title', (req, res) => {
+//   Book.findOneAndRemove({
+//     title: req.params.title
+//   })
+//         .then(() => {
+//           res.redirect('/books')
+//         })
+//         .catch((err) => {
+//           console.log(err)
+//         })
+// })
+
+// module.exports = Router
+
 const express = require('express')
 const Router = express.Router()
 const Book = require('../models/BookSchema')
 
 Router.get('/', (req, res) => {
   Book.find({})
-    .then((book) => {
-      res.json(book)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+   .then((book) => {
+     res.json(book)
+   })
+   .catch((err) => {
+     console.log(err)
+   })
 })
 
 Router.get('/:title', (req, res) => {
   Book.find({
-    title: req.params.title
-  })
-      .then((book) => {
-        res.json(book)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+   title: req.params.title
+ })
+     .then((book) => {
+       res.json(book)
+       // console.log(book)
+     })
+     .catch((err) => {
+       console.log(err)
+     })
 })
 
 Router.get('/:author', (req, res) => {
   Book.find({
-    author: req.params.author
-  })
-        .then((book) => {
-          res.json(book)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
+   author: req.params.author
+ })
+       .then((book) => {
+         res.json(book)
+       })
+       .catch((err) => {
+         console.log(err)
+       })
 })
 
 Router.post('/:title', (req, res) => {
   Book.create(req.body.book)
-          .then((book) => {
-            res.redirect(`/books/${book.title}`)
-          })
-          .catch((err) => {
-            console.log(err)
-          })
+         .then((book) => {
+           res.redirect(`/books/${book.title}`)
+         })
+         .catch((err) => {
+           console.log(err)
+         })
 })
 
 Router.put('/:title', (req, res) => {
   Book.findOneAndUpdate({
-    title: req.params.title
-  }, req.body.book, {new: true})
-        .then((book) => {
-          res.redirect(`/books/${book.title}`)
-        })
-        .catch((err) => {
-          console.log(err)
-        })
+   title: req.params.title
+ }, req.body.book, {new: true})
+       .then((book) => {
+         res.redirect(`/books/${book.title}`)
+       })
+       .catch((err) => {
+         console.log(err)
+       })
 })
 
 Router.delete('/:title', (req, res) => {
   Book.findOneAndRemove({
-    title: req.params.title
-  })
-        .then(() => {
-          res.redirect('/books')
-        })
-        .catch((err) => {
-          console.log(err)
-        })
+   title: req.params.title
+ })
+       .then(() => {
+         res.redirect('/books')
+       })
+       .catch((err) => {
+         console.log(err)
+       })
 })
 
 module.exports = Router
